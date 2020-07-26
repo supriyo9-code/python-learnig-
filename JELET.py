714816980ffb64f22174bccc03d37d1871415ea0
supriyo9-code <supriyomondal503@gmail.com>
2020-07-26 15:27:23 +0530

    JELET

--- JELET.py
+++ JELET.py
@@ -2,11 +2,13 @@ print("what is your GMR?")
 GMR = int(input())
 if GMR<=50:
     print("you can get any govt collge")
-if GMR>50 and GMR<=100:
+elif GMR>50 and GMR<=100:
     print("you get  govt college but chances of ju is less")
-if GMR>100 and GMR<=200:
+elif GMR>100 and GMR<=200:
     print("you can get govt college excluding JU")
-if GMR>200 and GMR<=500:
+elif GMR>200 and GMR<=500:
     print("you can get govt college excludig ju,kgec,jgec")
-elif(GMR>500) :
+elif GMR>500 and GMR<=10000:
     print("You only get a good private college")
+else:
+    print("better luck next time")
